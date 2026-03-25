c720c233f6ad95fa1f23304324f1ad8ef77c0cbf
Vinayak V Koni <vinayak>
2026-03-25 21:41:47 +0530

    deleted file

--- simple-java-app/simple-java-app/simple-java-app/README.md
+++
@@ -1,1 +0,0 @@
-# simple-java-app
--- simple-java-app/simple-java-app/simple-java-app/src/main/java/com/example/App.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Welcome to My Simple Java Application!");
-    }
-}
-
